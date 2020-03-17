5fe723144afb1e8c07f6e39126c597d25e7314fd
Coder-WS <wushuai686@163.com>
2020-03-17 16:59:08 +0800

    最近一次提交

--- js/common.js
+++ js/common.js
@@ -1,8 +1,8 @@
+// 顶部特效进度条
+NProgress.start();
+NProgress.done();
 
-	NProgress.start();
-
-	NProgress.done();
-
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+// 侧边栏伸缩
+$('.navs ul').prev('a').on('click', function () {
+  $(this).next().slideToggle();
+});
